6ca559a5b351ec48d8490e5d5a599314950605f5
Ricardo Santiago <ricardosantiagocwb@gmail.com>
2022-05-13 16:33:06 -0300

    Smart Contract

---
+++ Solidity/truffle-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  networks: {},
+  compilers: {
+    solc: {
+      settings: {
+        optimizer: {
+          enabled: true, // Default: false
+          runs: 200      // Default: 200
+        },
+      }
+    }
+  }
+};
