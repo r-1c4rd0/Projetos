53135a8711ed52957a5d123136733029b42895fa
ricardo <ricardosantiagocwb@gmail.com>
2024-01-23 17:21:12 -0300

    Push update test aplication

---
+++ credit-application-system/src/main/kotlin/me/dio/credit/application/system/CreditApplicationSystemApplication.kt
@@ -0,0 +1,12 @@
+package me.dio.credit.application.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditApplicationSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditApplicationSystemApplication>(*args)
+
+}
---
+++ credit-application-system/src/main/kotlin/me/dio/credit/application/system/enummeration/Status.kt
@@ -0,0 +1,6 @@
+package me.dio.credit.application.system.enummeration
+
+enum class Status {
+    IN_PROGRESS, APPROVED, REJECT
+
+}
