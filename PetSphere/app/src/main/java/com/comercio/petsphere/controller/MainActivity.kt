27a2aaffc1d667628c6af0d7da07abb58108e31c
ricardo <ricardo.silva@escriba.com.br>
2024-05-19 01:24:36 -0300

    Criando a MainActvity primeiros passos

--- PetSphere/app/src/main/java/com/comercio/petsphere/controller/MainActivity.kt
+++ PetSphere/app/src/main/java/com/comercio/petsphere/controller/MainActivity.kt
@@ -3,13 +3,13 @@ package com.comercio.petsphere.controller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.comercio.petsphere.ui.theme.PetSphereTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +17,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PetSphereTheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -30,17 +30,79 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MainScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Greeting("Pet Sphere")
+        FloatingActionButtons()
+    }
+}
+
+@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+    Row(
+
     )
+    {
+
+        Text(
+
+            text = "Bem vindos ao $name!",
+            modifier = modifier
+        )
+    }
+}
+
+@Composable
+fun FloatingActionButtons() {
+    Box (
+        contentAlignment = Alignment.BottomEnd,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Column(
+
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalAlignment = Alignment.End,
+
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            FloatingActionButton(
+                onClick = { /* TODO: Handle Veterinário click */ },
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Text(text = "Veterinário")
+            }
+            FloatingActionButton(
+                onClick = { /* TODO: Handle Nutrição click */ },
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Text(text = "Nutrição")
+            }
+            FloatingActionButton(
+                onClick = { /* TODO: Handle Nutrição click */ },
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Text(text = "Exercícios")
+            }
+            FloatingActionButton(
+                onClick = { /* TODO: Handle Perfil click */ },
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Text(text = "Perfil")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PetSphereTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
