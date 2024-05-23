0c6a5b8d0776fd789374383badcb720488141564
ricardo <ricardo.silva@escriba.com.br>
2024-05-23 00:30:04 -0300

    Refatorando o método floatbutton

--- PetSphere/app/src/main/java/com/comercio/petsphere/controller/MainActivity.kt
+++ PetSphere/app/src/main/java/com/comercio/petsphere/controller/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.comercio.petsphere.controller
 
-import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.comercio.petsphere.ui.theme.PetSphereTheme
 
@@ -18,7 +25,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PetSphereTheme {
-
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -32,13 +38,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    val buttons = listOf(
+        ButtonData(text = "Perfil", onClick = { /* TODO: Handle Perfil click */ }, padding = 18.dp),
+        ButtonData(text = "Exercícios", onClick = { /* TODO: Handle Exercícios click */ }, padding = 8.dp),
+        ButtonData(text = "Veterinário", onClick = { /* TODO: Handle Veterinário click */ }, padding = 8.dp),
+        ButtonData(text = "Nutrição", onClick = { /* TODO: Handle Nutrição click */ }, padding = 18.dp)
+    )
+
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        
         Greeting("Pet Sphere")
-        FloatingActionButtons()
-
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            contentAlignment = Alignment.BottomEnd
+        ) {
+            FloatingActionButtonGroup(buttons = buttons)
+        }
     }
 }
 
@@ -47,68 +65,36 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Box(
         contentAlignment = Alignment.TopCenter,
         modifier = modifier.fillMaxSize()
-    )
-    {
-
+    ) {
         Text(
-
             text = "Bem vindos ao $name!",
             modifier = modifier
         )
     }
 }
-@Composable
-fun Image(){
-    Box (){
-
-    }
-}
 
 @Composable
-fun FloatingActionButtons() {
-    Box (
-        contentAlignment = Alignment.BottomEnd,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
+fun FloatingActionButtonGroup(buttons: List<ButtonData>) {
+    Column(
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.spacedBy(16.dp) // Espacamento entre os botões
     ) {
-        Column(
-
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            horizontalAlignment = Alignment.End,
-
-            modifier = Modifier
-                .padding(6.dp)
-        ) {
+        buttons.forEach { button ->
             FloatingActionButton(
-                onClick = { /* TODO: Handle Perfil click */ },
+                onClick = button.onClick,
                 modifier = Modifier.align(Alignment.End)
             ) {
-                Text(text = "Perfil", modifier = Modifier.padding(18.dp))
+                Text(text = button.text, modifier = Modifier.padding(button.padding))
             }
-            FloatingActionButton(
-                onClick = { /* TODO: Handle Nutrição click */ },
-                modifier = Modifier.align(Alignment.End)
-            ) {
-                Text(text = "Exercícios", modifier = Modifier.padding(8.dp))
-            }
-            FloatingActionButton(
-                onClick = { /* TODO: Handle Veterinário click */ },
-                modifier = Modifier.align(Alignment.End)
-            ) {
-                Text(text = "Veterinário", modifier = Modifier.padding(8.dp))
-            }
-            FloatingActionButton(
-                onClick = { /* TODO: Handle Nutrição click */ },
-                modifier = Modifier.align(Alignment.End)
-            ) {
-                Text(text = "Nutrição", modifier = Modifier.padding(18.dp))
-            }
-
-
         }
     }
 }
+
+data class ButtonData(
+    val text: String,
+    val onClick: () -> Unit,
+    val padding: Dp
+)
 
 @Preview(showBackground = true)
 @Composable
